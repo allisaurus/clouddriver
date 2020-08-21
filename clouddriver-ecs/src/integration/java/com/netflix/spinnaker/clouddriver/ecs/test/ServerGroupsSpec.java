182d5b365fba039e34144d93c93d0bab743f48f7
Stanko <stankoa@amazon.com>
2020-08-21 16:33:08 -0700

    feat(test): Add initial ECS integration tests

---
+++ clouddriver-ecs/src/integration/java/com/netflix/spinnaker/clouddriver/ecs/test/ServerGroupsSpec.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.netflix.spinnaker.clouddriver.ecs.test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+
+import com.netflix.spinnaker.clouddriver.ecs.EcsSpec;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerGroupsSpec extends EcsSpec {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @DisplayName(".\n===\n" + "Given cached ECS service, retrieve it from /serverGroups" + "\n===")
+  @Test
+  public void getServerGroupsTest() {
+    // given
+    String url = getTestUrl("/serverGroups");
+
+    // when
+    Response response =
+        given()
+            .param("cloudProvider", "ecs")
+            .param("applications", "ecs")
+            .when()
+            .get(url)
+            .then()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            .extract()
+            .response();
+    log.info("SERVER GROUPS response:");
+    log.info(response.asString()); // returns empty list
+
+    // then
+    assertNotNull(response); // TODO inspect response contents
+  }
+}
